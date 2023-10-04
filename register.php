0942777c289351b2480e9ac4d82fc7bc4d782a65
Visionz73 <crazyvision26@gmail.com>
2023-10-04 10:06:22 +0200

    commit register

--- register.php
+++ register.php
@@ -1,40 +1,42 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+// Datenbankverbindung herstellen (verwenden Sie Ihre eigenen Zugangsdaten)
+$servername = "localhost";
+$username = "root";
+$password = "neues-passwort";
+$dbname = "Benutzeranmeldung";
 
-    // Fügen Sie hier Sicherheitsüberprüfungen und Validierung ein.
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Verbindung zur MySQL-Datenbank herstellen
-    $mysqli = new mysqli("localhost", "root", "neues-passwort", "Benutzeranmeldung");
-
-    // Überprüfen, ob die Verbindung erfolgreich hergestellt wurde
-    if ($mysqli->connect_error) {
-        die("Verbindungsfehler: " . $mysqli->connect_error);
-    }
-
-    // SQL-Abfrage zum Einfügen des Benutzers
-    $sql = "INSERT INTO benutzer (benutzername, passwort) VALUES (?, ?)";
-    
-    // Vorbereiten und ausführen der Abfrage
-    if ($stmt = $mysqli->prepare($sql)) {
-        // Passwort sicher hashen, bevor es in der Datenbank gespeichert wird
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-        
-        $stmt->bind_param("ss", $username, $hashedPassword);
-        
-        if ($stmt->execute()) {
-            echo "Benutzer wurde registriert.";
-        } else {
-            echo "Fehler bei der Benutzerregistrierung.";
-        }
-
-        $stmt->close();
-    } else {
-        echo "Fehler bei der Vorbereitung der SQL-Abfrage.";
-    }
-
-    // Datenbankverbindung schließen
-    $mysqli->close();
+// Überprüfen Sie die Verbindung auf Fehler
+if ($conn->connect_error) {
+    die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
 }
+
+// Benutzereingaben aus dem Formular abrufen
+$benutzername = $_POST["benutzername"];
+$passwort = $_POST["passwort"];
+
+// Überprüfen, ob der Benutzername bereits existiert
+$check_sql = "SELECT * FROM Benutzer WHERE Benutzername = '$benutzername'";
+$check_result = $conn->query($check_sql);
+
+if ($check_result->num_rows > 0) {
+    // Benutzername existiert bereits
+    echo "Der Benutzername ist bereits vergeben. Bitte wählen Sie einen anderen.";
+} else {
+    // Benutzer registrieren
+    // Passwort sicher hashen, bevor es in die Datenbank gespeichert wird
+    $hashedPassword = password_hash($passwort, PASSWORD_DEFAULT);
+    
+    $insert_sql = "INSERT INTO Benutzer (Benutzername, Passwort) VALUES ('$benutzername', '$hashedPassword')";
+
+    if ($conn->query($insert_sql) === TRUE) {
+        echo "Registrierung erfolgreich. Willkommen, $benutzername!";
+    } else {
+        echo "Fehler bei der Registrierung: " . $conn->error;
+    }
+}
+
+// Datenbankverbindung schließen
+$conn->close();
 ?>
