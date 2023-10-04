428952adea93b6f195f0d94422038ccee35b5141
Visionz73 <crazyvision26@gmail.com>
2023-10-04 11:42:12 +0200

    test

--- register.php
+++ register.php
@@ -1,3 +1,33 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style_login.css">
+    <title>Benutzerregistrierung</title>
+</head>
+<body>
+    <div class="container">
+        <div class="nav-menu">
+            <a href="landingpage.html">Home</a>
+            <a href="register.html">Registrieren</a>
+            <a href="login.html">Login</a>
+        </div>
+
+        <div class="login-box">
+            <h1>Benutzerregistrierung</h1>
+            <form action="register.php" method="post">
+                <label for="benutzername">Benutzername:</label>
+                <input type="text" id="benutzername" name="benutzername" required><br><br>
+
+                <label for="passwort">Passwort:</label>
+                <input type="password" id="passwort" name="passwort" required><br><br>
+
+                <input type="submit" value="Registrieren">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
 <?php
 // Datenbankverbindung herstellen (verwenden Sie Ihre eigenen Zugangsdaten)
 $servername = "localhost";
