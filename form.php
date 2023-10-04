6be1b1b7bf4e72c451b1193491a8ef94485deb04
Visionz73 <crazyvision26@gmail.com>
2023-10-04 12:09:11 +0200

    xcv

--- form.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Die Benutzereingabe aus dem Formular wird in der Variable $eingabe gespeichert
-    $benutzername = $_POST["Benutzername"];
-
-    // Jetzt können Sie die Variable $eingabe in Ihrem PHP-Code verwenden
-    echo "Ihre Eingabe: " . $benutzername;
-}
-?>
--- register.php
+++ register.php
@@ -1,24 +1,9 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $benutzername = $_POST["benutzername"];
-    $passwort = $_POST["passwort"];
-
-        if ($conn->query($insert_sql) === TRUE) {
-            echo "Registrierung erfolgreich. Willkommen, $benutzername!";
-        } else {
-            echo "Fehler bei der Registrierung: " 
-        }
-
-}
-
-?>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Die Benutzereingabe aus dem Formular wird in der Variable $eingabe gespeichert
-    $eingabe = $_POST["eingabe"];
+    $benutzername = $_POST["Benutzername"];
 
     // Jetzt können Sie die Variable $eingabe in Ihrem PHP-Code verwenden
-    echo "Ihre Eingabe: " . $eingabe;
+    echo "Ihre Eingabe: " . $benutzername;
 }
 ?>
