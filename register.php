22cc5012cfc71af6a874d4f15561e2c2cefb6537
Visionz73 <crazyvision26@gmail.com>
2023-10-04 14:05:11 +0200

    commit

--- register.php
+++ register.php
@@ -15,6 +15,12 @@ $Email = $_POST["Email"];
 // INSERT TO SQL
 $sql = "INSERT INTO Benutzer (Benutzername, Passwort, Email) VALUES ('$Benutzername', '$Passwort', '$Email')";
 
+if ($conn->query($sql) === TRUE) {
+    echo "Benutzerdaten wurden erfolgreich in die Datenbank eingefügt.";
+} else {
+    echo "Fehler beim Einfügen der Benutzerdaten: " . $conn->error;
+}
+
 // Überprüfen Sie die Verbindung auf Fehler
 
 if ($conn->connect_error) {
