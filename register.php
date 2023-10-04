af8dff23d5d825a1e0a8a169a388ec348bbd2f14
Visionz73 <crazyvision26@gmail.com>
2023-10-04 12:33:59 +0200

    bj

--- register.php
+++ register.php
@@ -9,23 +9,13 @@ $dbname = "Benutzeranmeldung";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Überprüfen Sie die Verbindung auf Fehler
-if ($conn->connect_error) {
-    die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $Benutzername = $_POST["Benutzername"];
-    $Passwort = $_POST["Passwort"];
-
-    // SQL-Abfrage zum Einfügen des Benutzers in die Datenbank
-    $sql = "INSERT INTO Benutzer (Benutzername, Passwort) VALUES ('$Benutzername', '$Passwort')";
 
     if ($conn->query($sql) === TRUE) {
         echo "Registrierung erfolgreich. Willkommen, $Benutzername $Passwort!";
     } else {
-        echo "Fehler bei der Registrierung: " . $conn->error;
+        echo "Fehler bei der Registrierung: " ;
     }
-}
+
 
 // Datenbankverbindung schließen
 $conn->close();
