c2ae8002e9072cace0fc674ecb1f6937f95481de
Visionz73 <crazyvision26@gmail.com>
2023-10-04 12:17:12 +0200

    sdf

--- register.php
+++ register.php
@@ -1,9 +1,31 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Die Benutzereingabe aus dem Formular wird in der Variable $eingabe gespeichert
     $benutzername = $_POST["Benutzername"];
+    $passwort = $_POST["Passwort"];
 
-    // Jetzt können Sie die Variable $eingabe in Ihrem PHP-Code verwenden
-    echo "Ihre Eingabe: " . $benutzername;
+    // Datenbankverbindung herstellen (verwenden Sie Ihre eigenen Zugangsdaten)
+    $servername = "localhost";
+    $username = "root";
+    $password = "neues-passwort";
+    $dbname = "Benutzeranmeldung";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Überprüfen Sie die Verbindung auf Fehler
+    if ($conn->connect_error) {
+        die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
+    }
+
+    // SQL-Abfrage zum Einfügen des Benutzers in die Datenbank
+    $sql = "INSERT INTO Benutzer (Benutzername, Passwort) VALUES ('$benutzername', '$passwort')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registrierung erfolgreich. Willkommen, $benutzername!";
+    } else {
+        echo "Fehler bei der Registrierung: " . $conn->error;
+    }
+
+    // Datenbankverbindung schließen
+    $conn->close();
 }
 ?>
