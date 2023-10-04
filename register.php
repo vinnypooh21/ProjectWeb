47dbbf655abc6221260ac99fba7742d2313b4221
Visionz73 <143585135+Visionz73@users.noreply.github.com>
2023-10-04 09:38:16 +0200

    Update register.php
    New register data

--- register.php
+++ register.php
@@ -1,39 +1,20 @@
 <?php
-// Datenbankverbindung herstellen (verwenden Sie Ihre eigenen Zugangsdaten)
-$servername = "localhost";
-$username = "root";
-$password = "neues-passwort";
-$dbname = "Benutzeranmeldung";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+    // Fügen Sie hier Sicherheitsüberprüfungen und Validierung ein.
 
-// Überprüfen Sie die Verbindung auf Fehler
-if ($conn->connect_error) {
-    die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
-}
+    // Erstellen Sie den Linux-Benutzer (hier als Beispiel mit `shell_exec`).
+    $command = "sudo useradd -m " . $username;
+    $output = shell_exec($command);
 
-// Benutzereingaben aus dem Formular abrufen
-$benutzername = $_POST["benutzername"];
-$passwort = $_POST["passwort"];
-
-// Überprüfen, ob der Benutzername bereits existiert
-$check_sql = "SELECT * FROM Benutzer WHERE Benutzername = '$benutzername'";
-$check_result = $conn->query($check_sql);
-
-if ($check_result->num_rows > 0) {
-    // Benutzername existiert bereits
-    echo "Der Benutzername ist bereits vergeben. Bitte wählen Sie einen anderen.";
-} else {
-    // Benutzer registrieren
-    $insert_sql = "INSERT INTO Benutzer (Benutzername, Passwort) VALUES ('$benutzername', '$passwort')";
-    
-    if ($conn->query($insert_sql) === TRUE) {
-        echo "Registrierung erfolgreich. Willkommen, $benutzername!";
+    if ($output !== null) {
+        echo "Benutzer wurde registriert.";
     } else {
-        echo "Fehler bei der Registrierung: " . $conn->error;
+        echo "Fehler bei der Benutzerregistrierung.";
     }
 }
-
-// Datenbankverbindung schließen
-$conn->close();
 ?>
+
+
