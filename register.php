a7bd73050652d8ac737d078b8bf72cf33ae8ebc4
Visionz73 <crazyvision26@gmail.com>
2023-10-04 12:27:13 +0200

    xdfg

--- register.php
+++ register.php
@@ -1,29 +1,36 @@
 <?php
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $benutzername = $_POST["Benutzername"];
     $passwort = $_POST["Passwort"];
+}
+// Datenbankverbindung herstellen (verwenden Sie Ihre eigenen Zugangsdaten)
+$servername = "localhost";
+$username = "root";
+$password = "neues-passwort";
+$dbname = "Benutzeranmeldung";
 
-    // Datenbankverbindung herstellen (verwenden Sie Ihre eigenen Zugangsdaten)
-    $servername = "localhost";
-    $username = "root";
-    $password = "neues-passwort";
-    $dbname = "Benutzeranmeldung";
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+// Überprüfen Sie die Verbindung auf Fehler
+if ($conn->connect_error) {
+    die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
+}
 
-    // Überprüfen Sie die Verbindung auf Fehler
-   
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $Benutzername = $_POST["benutzername"];
+    $Passwort = $_POST["passwort"];
 
     // SQL-Abfrage zum Einfügen des Benutzers in die Datenbank
-    $sql = "INSERT INTO Benutzer (Benutzername, Passwort) VALUES ('$benutzername', '$passwort')";
+    $sql = "INSERT INTO Benutzer (Benutzername, Passwort) VALUES ('$Benutzername', '$Passwort')";
 
     if ($conn->query($sql) === TRUE) {
-        echo "Registrierung erfolgreich. Willkommen, $benutzername!";
+        echo "Registrierung erfolgreich. Willkommen, $Benutzername!";
     } else {
         echo "Fehler bei der Registrierung: " . $conn->error;
     }
-
-    // Datenbankverbindung schließen
-    $conn->close();
 }
+
+// Datenbankverbindung schließen
+$conn->close();
 ?>
