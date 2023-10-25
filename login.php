3fbd1b82351bcfe37bcfcbd0e525220a7c9c03d1
Tammo <blueeyesyt1@gmail.com>
2023-10-25 11:13:30 +0200

    Server Benutzer erstellt nach login
    Berechtigungen die gesetzt werden müssen:
    1. sudo visudo
    2www-data ALL=(ALL) NOPASSWD: /usr/sbin/useradd

--- login.php
+++ login.php
@@ -13,25 +13,26 @@ if ($conn->connect_error) {
 }
 
 // Benutzereingaben aus dem Formular abrufen
-$Benutzername = $_POST["benutzername"];
+$benutzername = $_POST["benutzername"];
 $Passwort = $_POST["passwort"];
 
 
-        if (password_verify($Passwort, $hash)) {
-            echo "Das eingegebene Passwort ist korrekt.";
-        } else {
-            echo "Das eingegebene Passwort ist falsch.";
         }
+if (password_verify($Passwort, $hash)) {
+    echo "Das eingegebene Passwort ist korrekt.";
+} else {
+    echo "Das eingegebene Passwort ist falsch.";
+}
 
 
 // SQL-Abfrage zum Überprüfen der Anmeldedaten
-$sql = "SELECT * FROM Benutzer WHERE Benutzername = '$Benutzername' AND Passwort = '$Passwort'";
+$sql = "SELECT * FROM Benutzer WHERE benutzername = '$benutzername' AND Passwort = '$Passwort'";
 $result = $conn->query($sql);
 
 
 if ($result->num_rows == 1) {
     // Anmeldung erfolgreich
-    echo "Anmeldung erfolgreich. Willkommen, $Benutzername!";
+    echo "Anmeldung erfolgreich. Willkommen, $benutzername!";
 } else {
     // Anmeldung fehlgeschlagen
     echo "Anmeldung fehlgeschlagen. Überprüfen Sie Ihre Anmeldeinformationen.";
@@ -39,4 +40,21 @@ if ($result->num_rows == 1) {
 
 // Datenbankverbindung schließen
 $conn->close();
-?>
+
+$userExist = shell_exec('cat /etc/passwd');
+$userArray = explode(":", $userExist);
+// var_dump(shell_exec('cat /etc/passwd'));
+
+if (array_search($benutzername, $userArray)) {
+    echo '<a href="landingpage.html">';
+} else {
+    $linux_command = "sudo useradd -m -p" . " " . escapeshellarg($benutzername);
+    // exec($linux_command, $output, $return_var);
+    $linux_output = shell_exec($linux_command);
+    var_dump($linux_output);
+    if ($linux_output) {
+        die("Fehler beim Erstellen des Benutzers auf dem Linux-Server");
+    } else {
+        echo "Benutzer wurde erfolgreich erstellt";
+    }
+}
